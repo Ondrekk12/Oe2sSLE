441b2ecb0e7f959e09dc1fcaf57113005d7ac2f9
Ondrekk12 <pscondra@seznam.cz>
2024-07-22 22:06:00 +0000

    gg

--- utils.py
+++ utils.py
@@ -1,26 +1,3 @@
 # -*- coding: utf-8 -*-
 """
-Copyright (C) 2016-2017 Jonathan Taquet
 
-This file is part of Oe2sSLE (Open e2sSample.all Library Editor).
-
-Oe2sSLE is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Oe2sSLE is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Oe2sSLE.  If not, see <http://www.gnu.org/licenses/>
-"""
-
-def isint(obj):
-    try:
-        int(obj)
-        return True
-    except ValueError:
-        return False
